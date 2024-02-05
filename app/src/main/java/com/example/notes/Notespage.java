7727ea34a80144979546f275589e375292446098
ranganarayanan <ranganarayanana2002@gmail.com>
2024-02-05 16:45:15 +0530

    notes

--- app/src/main/java/com/example/notes/Notespage.java
+++ app/src/main/java/com/example/notes/Notespage.java
@@ -2,13 +2,24 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Notespage extends AppCompatActivity {
+    ImageView plusimg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notespage);
+        plusimg=(ImageView) findViewById(R.id.btnplus);
+        plusimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Notespage.this,Notestakepage.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/notes/Notestakepage.java
+++ app/src/main/java/com/example/notes/Notestakepage.java
@@ -2,13 +2,47 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class Notestakepage extends AppCompatActivity {
+    ImageView aroimage;
+    ImageView tcross;
+    ImageView mcross;
+    EditText tedittext;
+    EditText medittext;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notestakepage);
+        aroimage=(ImageView) findViewById(R.id.bckarrow);
+        aroimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        tedittext=(EditText)findViewById(R.id.etitle);
+        medittext=(EditText)findViewById(R.id.emessage);
+
+        tcross=(ImageView) findViewById(R.id.ettcross);
+        mcross=(ImageView) findViewById(R.id.etmcross);
+        tcross.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tedittext.setText("");
+            }
+        });
+        mcross.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                medittext.setText("");
+            }
+        });
     }
 }
