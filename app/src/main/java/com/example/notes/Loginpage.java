2414d0ecfd915d14cb5015de9aeb0340e45876ce
ranganarayanan <ranganarayanana2002@gmail.com>
2024-02-05 15:51:27 +0530

    notes

--- app/src/main/java/com/example/notes/Loginpage.java
+++ app/src/main/java/com/example/notes/Loginpage.java
@@ -3,13 +3,17 @@ package com.example.notes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Loginpage extends AppCompatActivity {
     CardView cardViewsignup;
+    Button signin;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +25,13 @@ public class Loginpage extends AppCompatActivity {
                 startActivity(new Intent(Loginpage.this, Signuppage.class));
             }
         });
+        signin=(Button) findViewById(R.id.btnsignin);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Loginpage.this,Notespage.class));
+            }
+        });
+
     }
 }
